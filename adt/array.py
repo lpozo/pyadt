2c7ea8bcbd2a07b38d0a83964a12a0ee965fe503
Leodanis Pozo Ramos <lpozor78@gmail.com>
2021-07-06 22:32:35 -0400

    Refactor array.py
    - Move index validation code to utils.py
    - Turn some methods into convenient properties
    - Add docstrings with doctests
    - Change from LBYL to EAFP
    - Remove Array2DList

--- adt/array.py
+++ adt/array.py
@@ -2,7 +2,9 @@
 
 import ctypes
 from collections import namedtuple
-from typing import Any, Iterator, Tuple
+from typing import Any, Generator, Iterator, Tuple
+
+from adt.utils import validate_index
 
 
 class Array:
@@ -25,10 +27,11 @@ class Array:
         self._data = (ctypes.py_object * self._size)()
         self.clear()
 
+    @property
     def capacity(self) -> int:
         """Return the array's capacity.
 
-        >>> Array(20).capacity()
+        >>> Array(20).capacity
         20
         """
         return self._size
@@ -51,18 +54,19 @@ class Array:
     def __len__(self) -> int:
         return len(self._data)
 
-    def __getitem__(self, index) -> Any:
-        if 0 <= index < self._size:
+    def __getitem__(self, index: int) -> Any:
+        try:
             return self._data[index]
-        raise IndexError(f"Index out of range: {index}")
+        except IndexError:
+            raise IndexError(f"Index out of range: {index}") from None
 
-    def __setitem__(self, index, value) -> None:
-        if 0 <= index < self._size:
+    def __setitem__(self, index: int, value: Any) -> None:
+        try:
             self._data[index] = value
-        else:
-            raise IndexError(f"Index out of range: {index}")
+        except IndexError:
+            raise IndexError(f"Index out of range: {index}") from None
 
-    def __iter__(self):
+    def __iter__(self) -> Generator[Any, None, None]:
         yield from self._data
 
     def __str__(self) -> str:
@@ -72,33 +76,73 @@ class Array:
         return f"{self.__class__.__name__}(size={self._size})"
 
 
+Capacity = namedtuple("Capacity", "rows cols items")
+Data = namedtuple("Data", "row col data")
+
+
 class Array2D:
-    Capacity = namedtuple("Capacity", "rows columns items")
-    Data = namedtuple("Data", "row column data")
+    """2d array abstract data type based on ctypes.py_object.
+
+    >>> a = Array2D(2, 4)
+    >>> a
+    Array2D(rows=2, cols=4)
+    >>> a[0, 0] = 42
+    """
 
     def __init__(self, rows: int, cols: int) -> None:
-        self._table = self.Capacity(rows, cols, rows * cols)
+        self._table = Capacity(rows, cols, rows * cols)
         self._data = [(ctypes.py_object * cols)() for _ in range(rows)]
         self.clear()
 
+    @property
     def rows(self) -> int:
+        """Return the number of rows in the array.
+
+        >>> a = Array2D(2, 4)
+        >>> a.rows
+        2
+        """
         return self._table.rows
 
-    def columns(self) -> int:
-        return self._table.columns
+    @property
+    def cols(self) -> int:
+        """Return the number of columns in the array.
+
+        >>> a = Array2D(2, 4)
+        >>> a.cols
+        4
+        """
+        return self._table.cols
+
+    @property
+    def capacity(self) -> "Capacity":
+        """Return the capacity of the array.
+
+        >>> a = Array2D(2, 4)
+        >>> a.capacity
+        Capacity(rows=2, cols=4, items=8)
+        """
+        return self._table
 
     def clear(self) -> None:
+        """Clear the array by setting all the items to None.
+
+        >>> a = Array2D(2, 2)
+        >>> a[0, 0] = 42
+        >>> a[0, 0]
+        42
+        >>> a.clear()
+        >>> a[0, 0] is None
+        True
+        """
         for row in range(self._table.rows):
-            for col in range(self._table.columns):
+            for col in range(self._table.cols):
                 self[row, col] = None
 
     def __iter__(self) -> Iterator["Data"]:
         for row in range(self._table.rows):
-            for col in range(self._table.columns):
-                yield self.Data(row, col, self[row, col])
-
-    def capacity(self) -> "Capacity":
-        return self._table
+            for col in range(self._table.cols):
+                yield Data(row, col, self[row, col])
 
     def __str__(self) -> str:
         return (
@@ -107,72 +151,19 @@ class Array2D:
             + "\n)"
         )
 
-    def __getitem__(self, index: Tuple[int, int]) -> Any:
-        row, col = self._validate_indices(index)
-        return self._data[row][col]
-
-    def __setitem__(self, index: Tuple[int, int], value: Any) -> None:
-        row, col = self._validate_indices(index)
-        self._data[row][col] = value
-
-    def _validate_indices(self, index: Tuple[int, int]) -> Tuple[int, int]:
-        if not isinstance(index, tuple) or len(index) != 2:
-            raise IndexError("Invalid number of indices")
-        row, col = index
-        if not 0 <= row < self._table.rows:
-            raise IndexError("Row index out of range")
-        if not 0 <= col < self._table.columns:
-            raise IndexError("Column index out of range")
-        return index
-
-    def __len__(self) -> int:
-        return self._table.items
-
-
-class Array2DList:
-    Capacity = namedtuple("Capacity", "rows columns items")
-
-    def __init__(self, rows: int, cols: int) -> None:
-        self._table = self.Capacity(rows, cols, rows * cols)
-        self._data = [[None] * cols for _ in range(rows)]
-
-    @property
-    def rows(self) -> int:
-        return self._table.rows
-
-    @property
-    def columns(self) -> int:
-        return self._table.columns
-
-    def clear(self) -> None:
-        for row in range(self._table.rows):
-            for col in range(self._table.columns):
-                self[row, col] = None
-
-    def capacity(self) -> "Capacity":
-        return self._table
-
-    def __str__(self) -> str:
+    def __repr__(self) -> str:
         return (
-            f"{self.__class__.__name__}(\n\t"
-            + "\n\t".join(str(row) for row in self._data)
-            + "\n)"
+            f"{self.__class__.__name__}"
+            + f"(rows={self.rows}, cols={self.cols})"
         )
 
     def __getitem__(self, index: Tuple[int, int]) -> Any:
-        row, col = self._validate_indices(index)
+        row, col = validate_index(index, self.rows, self.cols)
         return self._data[row][col]
 
     def __setitem__(self, index: Tuple[int, int], value: Any) -> None:
-        row, col = self._validate_indices(index)
+        row, col = validate_index(index, self.rows, self.cols)
         self._data[row][col] = value
 
-    def _validate_indices(self, index: Tuple[int, int]) -> Tuple[int, int]:
-        if not isinstance(index, tuple) or len(index) != 2:
-            raise IndexError("Invalid number of indices")
-        row, col = index
-        if not 0 <= row < self._table.rows:
-            raise IndexError("Row index out of range")
-        if not 0 <= col < self._table.columns:
-            raise IndexError("Column index out of range")
-        return index
+    def __len__(self) -> int:
+        return self._table.items
