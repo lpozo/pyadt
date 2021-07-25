e128b85d27d5afe3aff52a1e49db17d5577ab0e5
Leodanis Pozo Ramos <lpozor78@gmail.com>
2021-07-24 21:15:38 -0400

    Add test for array.py

--- tests/test_array.py
+++ tests/test_array.py
@@ -2,4 +2,31 @@
 
 import pytest
 
-from pyadt import Array, Array2D
+from pyadt import Array
+
+
+def test_build():
+    a = Array(5)
+    assert len(a) == 5
+
+
+def test_assignment():
+    a = Array(5)
+    with pytest.raises(IndexError):
+        a[10] = 42
+
+
+def test_clear():
+    a = Array(5)
+    for i in range(len(a)):
+        a[i] = 1
+    assert sum(a) == 5
+    a.clear()
+    assert not any(a)
+
+
+def test_reversed():
+    a = Array(5)
+    for i in range(len(a)):
+        a[i] = i
+    assert list(reversed(a)) == [4, 3, 2, 1, 0]
